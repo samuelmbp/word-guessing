5d10662a0181979f243676d74319957a85082ff3
Samuel Raducan <samuel.thembp@gmail.com>
2022-09-19 12:33:03 +0100

    Create constructor for the game class and getWordToGuess method shows only the first letter

--- app/src/main/java/game/Game.java
+++ app/src/main/java/game/Game.java
@@ -1,12 +1,20 @@
 package game;
 
 public class Game {
+	String word;
+
+	public Game(String word) {
+		this.word = word;
+	}
 
 	public static void main(String[] args) {
-
 	}
 
 	public String getWordToGuess() {
-		return "B____";
+		StringBuilder sb = new StringBuilder(word);
+		for (int i = 1; i < word.length(); i++) {
+			sb.replace(i, word.length(), "_");
+		}
+		return sb.toString();
 	}
 }
--- app/src/test/java/game/GameTest.java
+++ app/src/test/java/game/GameTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 public class GameTest {
 	@Test
 	public void testGetsWordToGuess() {
-		Game game = new Game();
-		assertEquals(game.getWordToGuess(), "B____");
+		Game game = new Game("MAKERS");
+		assertEquals(game.getWordToGuess(), "M_____");
 	}
 }
